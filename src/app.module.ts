835ed2f19279ee37a8545c3128f5a3fbd95e5ae8
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-08-24 09:44:34 +0200

    config typeOrm et dotenv ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      // Endroit ou il faut mettre toutes les entités pour que typeOrm les prennent
+      // en compte.
+      synchronize: false,
+      // "synchronize" doit rester sur false. Lorsqu'il est activé (true), TypeORM essaiera d'ajuster automatiquement les tables
+      // dans la base de données en fonction de vos entités. Cela pourrait entraîner des changements non désirés.
+      // Nous voulons avoir un contrôle total sur la structure de notre base de données.
+      dropSchema: false,
+      // Important de laisser "dropSchema" sur false. Si mis à true,
+      // cela supprimera et recréera les tables lors de l'initialisation,
+      // effaçant toutes les données.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+// ------------------------------------------
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+// Plusieurs console Log pour verifier que le fichier d'environnement
+// est bien pris en compte
+// ------------------------------------------
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
