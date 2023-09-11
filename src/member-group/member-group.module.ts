64606a1fc22d2e77001f182a48b9d9154405aba6
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-09-11 16:19:59 +0200

    suppression de membergroupe

--- src/member-group/member-group.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MemberGroupService } from './member-group.service';
-import { MemberGroupController } from './member-group.controller';
-
-@Module({
-  controllers: [MemberGroupController],
-  providers: [MemberGroupService],
-})
-export class MemberGroupModule {}
