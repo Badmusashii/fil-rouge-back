35dc978c6121c48481747e565a6f193e0b29440f
Badmusashii <badmusashii@gmail.com>
2023-09-28 20:45:30 +0200

    update member ok

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -2,7 +2,7 @@ import { IsString, IsBoolean, IsInt } from 'class-validator';
 
 export class CreateReviewDto {
   @IsString()
-  review: string;
+  review?: string;
 
   @IsBoolean()
   vote: boolean;
